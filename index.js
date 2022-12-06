b8402882f9dca41dbd5e57085034c928e46572d7
Zac Marcengill <zacmarcengill@gmail.com>
2022-12-06 17:51:32 -0600

    Getting Articles from Website

--- index.js
+++ index.js
@@ -5,23 +5,39 @@ const express = require('express')
 
 const app = express()
 
-const url = 'https://www.tva.com/environment/lake-levels/center-hill'
+const url = 'https://www.theguardian.com/us'
 
 axios(url)
     .then(response => {
         const html = response.data
         const $ = cheerio.load(html)
-        const tvawidgetArr = []
-
-        $('.tvawidget', html).each(function() {
-            const h2Text = $(this).find('h2').text()
-            // const links = $(this).find('a').attr('href')
-            tvawidgetArr.push({
-                h2Text,
+        const articles = []
+        $('.fc-item__title', html).each(function() {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
             })
         })
-        console.log('tvawidgetArr: ' + tvawidgetArr)
+        console.log(articles)
     }).catch(err => console.log(err))
+
+// axios(url)
+//     .then(response => {
+//         const html = response.data
+//         const $ = cheerio.load(html)
+//         const tvawidgetArr = []
+
+//         $('.tvawidget', html).each(function() {
+//             const h2Text = $(this).find('h2').text()
+//             // const links = $(this).find('a').attr('href')
+//             tvawidgetArr.push({
+//                 h2Text,
+//             })
+//         })
+//         console.log('tvawidgetArr: ' + tvawidgetArr)
+//     }).catch(err => console.log(err))
 
 app.listen(PORT, () => console.log('server running on PORT ${PORT}'))
 
